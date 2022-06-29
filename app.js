915d7b36040368eb9bfae7d656593df3ed80e438
unknown <maroun233243@gmail.com>
2022-06-29 12:01:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const CW = document.querySelector('[CW-input]');
+const LBP = document.querySelector(".LBPinner");
+const BW = document.querySelector(".BWinner")
+const buttons = [...document.querySelectorAll(".button")];
+function convertCW(){
+CW.addEventListener('input', function(e) {
+    const cw = e.target.value;
+    const LBPresult = cw * 60000;
+    const x = LBPresult.toLocaleString("en-US");
+    const BWresult = cw * 1.6;
+    const y = BWresult.toFixed(2);
+    BW.innerHTML = y;    
+    LBP.innerHTML = x;
+    buttons.forEach(button =>{
+    button.addEventListener('click', event =>{
+        const value = event.target.innerHTML.replace('%', '');
+        const newValue = value / 100;
+        const result = parseInt(x) * 1000 * (1 - newValue) ;
+        const finalResult = result.toLocaleString("en-US");
+        console.log(result)
+        LBP.innerHTML = finalResult;
+    });
+    });
+});   
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    convertCW();
+    
+});
